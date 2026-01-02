d8c5a8c514a86c8e523fe4b4631e90a2e88ecc7d
karan-jadhav4961 <karan.jadhav4961@gmail.com>
2026-01-02 15:39:33 +0530

    Refactor comments and fix typos in tcp_client.py
    Updated comments for clarity and fixed minor typos.

--- tcp_client.py
+++ tcp_client.py
@@ -1,5 +1,6 @@
 ''' 
-This is a simple TCP client script in Python that connects to a server running on 127.0.0.1 (localhost) at port 9998. The script performs the following tasks:
+This is a simple TCP client script in Python that connects to a server running on 127.0.0.1 (localhost) at port 9998. That send some data 
+to the sever and receiv data from the server and print it.
 '''
 
 import socket
@@ -7,16 +8,11 @@ import socket
 target_host = "127.0.0.1"  # Server's IP address
 target_port = 9998         # Must match the server's port
 
-# Create a socket and connect to the server
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((target_host, target_port))
 
-# Send a message to the server
-client.send(b"Hello, Server! hii brother dfjkglgjldj")
-
-# Receive the server's response
-response = client.recv(4096)
-print(f"Server response: {response.decode('utf-8')}")
-
-# Close the connection
-client.close()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # Create an IPv4 TCP socket
+client.connect((target_host, target_port))                    # This is 3-way handshaking, by the way client send SYN, server reply
+                                                              # SYN+ACK, and again client sent ACK
+client.send(b"Hello, Server! hii brother dfjkglgjldj")        # Send data to server in byte to unserstand every machines
+response = client.recv(4096)                                  # Receive the server's response
+print(f"Server response: {response.decode('utf-8')}")         # Print the server response
+client.close()                                                # Close the connection
